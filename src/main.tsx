0f6f0698c122d64083997e67a5d390556f57e492
Acalculiaux <1455384013@qq.com>
2026-05-21 16:22:32 +0800

    fix: use HashRouter and add Buffer polyfill to resolve blank page

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Buffer } from 'buffer'
 import App from './App.tsx'
 import './styles/global.css'
+
+// Polyfill Buffer for gray-matter/browser compatibility
+window.Buffer = Buffer
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
